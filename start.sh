61052b06555003da4c45e122271528bb8a37b34b
Jim Holmstrom <jim.holmstroem@gmail.com>
2017-09-18 17:49:29 -0400

    Minor update to start.sh

--- start.sh
+++ start.sh
@@ -5,22 +5,33 @@ docker --version
 
 REMOTE=https://github.com/Stockholm-AI/stockholm-ai
 BRANCH=master
-VOLUME_NAME=stockholm-ai-volume-$BRANCH
+
+SOURCE_VOLUME=stockholm-ai-source-$BRANCH
 
 SYNCER_NAME=stockholm-ai-syncer-$BRANCH
 SYNCER_IMAGE=indiehosters/git:latest
 
 SERVER_NAME=stockholm-ai-server-$BRANCH
-SERVER_IMAGE=jekyll/jekyll:3.4
+SERVER_IMAGE=jekyll/jekyll:latest
 
 function cleanup {
     docker rm -f $SYNCER_NAME || true
     docker rm -f $SERVER_NAME || true
-    docker volume rm $VOLUME_NAME || true
+    docker volume rm $SOURCE_VOLUME || true
 } && cleanup && trap cleanup EXIT
 
-docker volume create --name $VOLUME_NAME
+docker volume create --name $SOURCE_VOLUME
 
-docker run --detach --volume $VOLUME_NAME:/volume --name $SYNCER_NAME $SYNCER_IMAGE /bin/sh -c "git clone $REMOTE /volume && while true; do git pull && sleep 1; done"
+docker run \
+    --detach \
+    --volume $SOURCE_VOLUME:/source \
+    --name $SYNCER_NAME $SYNCER_IMAGE \
+    /bin/sh -c "git clone $REMOTE /source && cd /source && git checkout $BRANCH && while true; do git pull && sleep 1; done"
 
-docker run --publish 4000:4000 --interactive --tty --volume $VOLUME_NAME:/volume --name $SERVER_NAME $SERVER_IMAGE /bin/sh -c "jekyll serve --destination /tmp/site --source /volume"
+docker run \
+    --publish 80:4000 \
+    --interactive \
+    --tty \
+    --volume $SOURCE_VOLUME:/source \
+    --name $SERVER_NAME $SERVER_IMAGE \
+    /bin/sh -c "jekyll serve --source /source --destination /srv/jekyll"
