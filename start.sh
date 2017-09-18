9f3a60bc816b7272fa130938a66f061ca444b173
Jim Holmstrom <jim.holmstroem@gmail.com>
2017-09-18 16:31:47 -0400

    Trying to fix CD

--- start.sh
+++ start.sh
@@ -17,10 +17,10 @@ function cleanup {
     docker rm -f $SYNCER_NAME || true
     docker rm -f $SERVER_NAME || true
     docker volume rm $VOLUME_NAME || true
-} && trap cleanup EXIT
+} && cleanup && trap cleanup EXIT
 
 docker volume create --name $VOLUME_NAME
 
-(docker run --detach --volume $VOLUME_NAME:/volume --name $SYNCER_NAME $SYNCER_IMAGE /bin/sh -c "git clone $REMOTE /volume && while true; do git pull && sleep 1; done" || kill 0)&
+docker run --detach --volume $VOLUME_NAME:/volume --name $SYNCER_NAME $SYNCER_IMAGE /bin/sh -c "git clone $REMOTE /volume && while true; do git pull && sleep 1; done"
 
-docker run --publish 80:4000 --interactive --tty --volume $VOLUME_NAME:/volume --name $SERVER_NAME $SERVER_IMAGE /bin/sh -c "jekyll serve --destination /tmp/site --source /volume"
+docker run --publish 4000:4000 --interactive --tty --volume $VOLUME_NAME:/volume --name $SERVER_NAME $SERVER_IMAGE /bin/sh -c "jekyll serve --destination /tmp/site --source /volume"
