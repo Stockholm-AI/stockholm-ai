451f05c6bc9e2e01d3867c2452e174561bde92a3
Jim Holmstrom <jim.holmstroem@gmail.com>
2017-09-26 16:00:06 -0400

    First iteration of mailchimp mailing list

--- start.sh
+++ start.sh
@@ -15,28 +15,15 @@ SYNCER_IMAGE=indiehosters/git:latest
 SERVER_NAME=stockholm-ai-server-$BRANCH
 SERVER_IMAGE=jekyll/jekyll:builder
 
-API_NAME=stockholm-ai-api
-API_IMAGE=python:alpine
-
-PROXY_NAME=stockholm-ai-proxy
-PROXY_IMAGE=nginx:alpine
-
-NETWORK=stockholm-ai-network
 
 function cleanup {
     docker rm -f $SYNCER_NAME || true
-    docker rm -f $PROXY_NAME || true
-    docker rm -f $MAILING_LIST_PROXY_NAME || true
     docker rm -f $SERVER_NAME || true
     docker volume rm $SOURCE_VOLUME || true
-    docker volume rm $DESTINATION_VOLUME || true
-    docker network rm $NETWORK || true
 } && cleanup && trap cleanup EXIT
 
 docker volume create --name $SOURCE_VOLUME
-docker volume create --name $DESTINATION_VOLUME
 
-docker network create --driver bridge $NETWORK
 
 docker run \
     --detach \
@@ -48,33 +35,12 @@ docker run \
 docker run \
     --detach \
     --restart unless-stopped \
-    --publish 80:80 \
-    --volume $DESTINATION_VOLUME:/usr/share/nginx/html \
-    --name $PROXY_NAME \
-    $PROXY_IMAGE
-
-docker run \
-    --detach \
-    --restart unless-stopped \
-    --publish 8000:8000 \
-    --network $NETWORK \
-    --volume `pwd`/_api/api.py:/api.py \
-    --name $API_NAME \
-    $API_IMAGE \
-    /bin/sh -c "pip install requests Flask && python -m http.server"
-
-
-docker run \
-    --detach \
-    --restart unless-stopped \
-    --network $NETWORK \
+    --publish 80:4000 \
     --volume $SOURCE_VOLUME:/source \
-    --volume $DESTINATION_VOLUME:/destination \
-    --volume `pwd`/nginx_conf:/etc/nginx:ro \
     --name $SERVER_NAME \
     --user root \
     $SERVER_IMAGE \
-    /bin/sh -c "[[ -f /source/_config.yml ]] && jekyll build --watch --source /source --config /source/_config.yml --destination /destination"
+    /bin/sh -c "[[ -f /source/_config.yml ]] && jekyll serve --source /source --config /source/_config.yml --destination /destination"
 
 
 sleep infinity
