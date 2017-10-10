8b22c7c72e4577f744b0a47bb7e309ede83e9327
Jim Holmstrom <jim.holmstroem@gmail.com>
2017-10-10 17:40:52 -0400

    First iteration of simple starter

--- simple_start.sh
+++ simple_start.sh
@@ -1,9 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
+
+printf "Starting nginx .. "
+nginx -p `pwd` -c static_server/nginx.conf
+printf "Done\n"
+
+function stop_nginx {
+    printf "Stopping nginx .. "
+    kill "$(cat /tmp/nginx.pid)"
+    printf "Done\n"
+} && trap stop_nginx EXIT
 
 while :
 do git fetch \
-    && [[ -n "$(git diff master origin/master)" ]] \
+    && [[ $(git diff master origin/master) ]] \
     && git pull \
     && jekyll build
 sleep 1
